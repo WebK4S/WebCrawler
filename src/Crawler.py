4bbb93825db4c83eb9ec4fa5e083f650859fae2c
Kamil Ciepluch <kamil.ciepluch@syncron.com>
2018-01-04 13:45:38 +0100

    Crawler class

--- src/Crawler.py
+++ src/Crawler.py
@@ -2,6 +2,19 @@ import urllib3
 import sqlite3
 import html
 import urllib
+import HTMLParser
 from urllib import parse
 
-class Crawler():
+class HREFParser(HTMLParser):
+    """
+    Extracting hrefs
+    """
+class Crawler:
+
+    href = set()
+    html = ''
+
+    def __init__(self, html):
+
+        self.html = html
+
