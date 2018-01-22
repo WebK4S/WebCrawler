d52a895f3460cb7195f9f62663ef77a148d0f99f
Kamil Ciepluch <kamil.ciepluch@syncron.com>
2018-01-22 10:29:18 +0100

    DB connection added, part of Crawler class

--- src/Crawler.py
+++ src/Crawler.py
@@ -23,12 +23,77 @@ class HREFParser(HTMLParser):
         hrefs = set()
         parser = HTMLParser
         parser.feed(html)
-class Crawler_SQLite:
+        for href in parser.hrefs:
+            u_parse = urlparse(href)
+            if href.startswith('/'):
+                hrefs.add(u_parse.path)
+            else:
+                if u_parse.netloc == domain :
+                    hrefs.add(u_parse.path)
+        return hrefs
 
-    hrefs = set()
-    html = ''
+"""
+Class resposible for connection with database, and data transfering
+"""
 
-    def __init__(self, html):
 
-        self.html = html
+class Crawler_SQLite(object):
+
+
+    def __init__(self, db_file):
+        self.conn = sqlite3.connect(db_file)
+        c = self.conn.cursor()
+        query = '''CREATE TABLE IF NOT EXIST sites (domain text, url text, content text)'''
+        c.execute(query)
+        self.conn.commit()
+        self.cursor = self.conn.cursor()
+
+    def set(self,domain, url, data):
+        query = "INSERT INTO sites VALUES(?,?,?)"
+        self.cursor.execute(query,(domain,url,data))
+        self.conn.commit()
+
+    def get(self,domain,url):
+        query = "SELECT content FROM site WHERE domain=? AND url =?"
+        self.cursor.execute(query,(domain,url))
+        row = self.cursor.fetchone()
+        if row:
+            return row[0]
+
+    def get_urls(self,domain):
+        query = "SELECT url FROM sites WHERE domain=?"
+        self.cursor.execute(query, (domain,))
+
+        return [row[0] for row in self.cursor.fetchall()]
+
+"""
+Main crawler class
+"""
+class Crawler(object):
+    """
+    cache - basic controller
+    depth - how deep we need to crawl page
+    """
+    def __init__(self, cache=None, depth=2):
+        self.depth = depth
+        self.cache=cache
+        self.content = {}
+
+    """
+    One of basic method, 
+    url - should be full url adress 
+    no_cache - function returning True if the web page should be refreshed
+    """
+    def crawl(self,url, no_cache=None):
+        u_parse = urlparse(url)
+        self.domain = u_parse.netloc
+        self.content[self.domain] = {}
+        self.scheme = u_parse.scheme
+        self.no_cache = no_cache
+        self._crawl([u_parse.path], self.depth)
+
+    def set(self, url, html):
+
+
+
 
